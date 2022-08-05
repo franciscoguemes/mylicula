e91e2864b6cafd5aeb0b81f674881c44a2dd61d8
Francisco <francisco@franciscoguemes.com>
2022-08-06 01:41:16 +0300

    Re-structure project

--- customize/ubuntu/install_icons.sh
+++ customize/ubuntu/install_icons.sh
@@ -12,9 +12,43 @@
 #                 
 ####################################################################################################
 
-#TODO: Copy the icons from ./resources/images/icons to ~/Pictures/Ubuntu_customization
+DESTINATION_DIR=${HOME}/Pictures/Ubuntu_customization/icons
+mkdir -p ${DESTINATION_DIR}
+
+# Get the directory where this script is placed
+#   https://stackoverflow.com/a/246128
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ICONS_DIR=${SCRIPT_DIR}/resources/images/icons
 
 
-#TODO: Setup the icons
+#-----------------------------------------------------------------------------
+# Create symbolic links to the icons
+#-----------------------------------------------------------------------------
+#TODO: Move this to a function
+#TODO: Make this functionality bulletproof `ln -s kk kk` to Too many levels of symbolic links
+echo "Installing icons..."
+for f in ${ICONS_DIR}/*
+do
+  FILE_NAME=$(basename $f)
+  echo "    ${FILE_NAME}"
+  LINK=${DESTINATION_DIR}/${FILE_NAME}
+#   if [[ -f ${LINK} ]]; then
+#     rm -f ${LINK}
+#   fi
+  ln -fs $f ${LINK}
+done
 
 
+#-----------------------------------------------------------------------------
+# Set the icons to the directories
+#-----------------------------------------------------------------------------
+# See: https://forums.linuxmint.com/viewtopic.php?t=352261
+#
+# gio set $DIRECTORY -t unset metadata::custom-icon
+# gio set "${HOME}/Documents/Nextcloud" -t unset metadata::custom-icon
+
+echo "${HOME}/Documents/Nextcloud"
+echo "${DESTINATION_DIR}/Nextcloud_directory.png"
+gio set "${HOME}/Documents/Nextcloud" -t string metadata::custom-icon "${DESTINATION_DIR}/Nextcloud_directory.png"
+# gio set "${HOME}/Documents/Nextcloud" -t string metadata::custom-icon "${ICONS_DIR}/Nextcloud_directory.png"
--- customize/ubuntu/install_packages.sh
+++
@@ -1,4 +0,0 @@
-
-
-# read the packages to install from ./resources/list_of_packages.txt
-
