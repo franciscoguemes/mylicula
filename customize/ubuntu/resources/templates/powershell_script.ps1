6b1d701627c2e754519b31de16fbca0dd54e73e0
franciscoguemes <francisco.jose.guemes.sevilla@gmail.com>
2025-03-25 18:56:03 +0200

    Add first draft for powershell_script example

---
+++ customize/ubuntu/resources/templates/powershell_script.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    Short description of what the script does.
+
+.DESCRIPTION
+    Detailed description of the script, including its purpose and functionality.
+
+.PARAMETER FirstArgument
+    Description of the first argument.
+
+.PARAMETER SecondArgument
+    Description of the second argument.
+
+.PARAMETER NthArgument
+    Description of the n-th argument.
+
+.EXAMPLE
+    Typical example of usage of the script.
+    Example: .\YourScript.ps1 -FirstArgument value1 -SecondArgument value2
+
+.OUTPUTS
+    [OutputType] Description of what the script outputs to stdout.
+    [OutputType] Description of what the script outputs to stderr.
+
+.RETURN
+    Description of the return codes and what they signify.
+
+.AUTHOR
+    Francisco Güemes
+
+.EMAIL
+    francisco@franciscoguemes.com
+
+.LINK
+    https://stackoverflow.com/questions/14008125/shell-script-common-template
+    https://devhints.io/powershell
+    https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/powershell-scripting-best-practices?view=powershell-7.2
+    https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-powershell
+
+#>
